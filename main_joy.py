09c79dddf2b9ac938629b470a610e3fcab5fd69d
lorszil <lorszil@gmail.com>
2016-09-17 10:54:57 +0200

    menu joystick test

--- main_joy.py
+++ main_joy.py
@@ -159,220 +159,210 @@ class Wall(pygame.sprite.Sprite):
 
 
 
+def main():
 
+    BackGround = Background("./pictures/background.jpg", [-100, -100])
 
-BackGround = Background("./pictures/background.jpg", [-100, -100])
+    pygame.init()
 
-pygame.init()
-
-screen_width = 1280
-screen_height = 800
-screen = pygame.display.set_mode([screen_width, screen_height])
-
-
-
+    screen_width = 1280
+    screen_height = 800
+    screen = pygame.display.set_mode([screen_width, screen_height])
 
 
 
+    wall_list = pygame.sprite.Group()
+    all_sprites_list = pygame.sprite.Group()
+    p1_bullet = pygame.sprite.Group()
+    p2_bullet = pygame.sprite.Group()
+    bullet_list = pygame.sprite.Group()
+
+    player = Player(50,screen_height/2, [0, 4], 1)
+    player2 = Player(screen_width-50,screen_height/2, [180, 4], 2)
+    all_sprites_list.add(player,player2)
+
+
+    rock_wall = Wall(screen_width/2, screen_height/2,100,100)
+    wall_list.add(rock_wall)
+    all_sprites_list.add(rock_wall)
+
+
+    done = False
+
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+    pygame.key.set_repeat(50,50)
+
+    score = 0
+
+    # sound
+    shoot_snd = pygame.mixer.Sound(path.join(snd_dir, 'shoot.wav'))
+    boom_snd = pygame.mixer.Sound(path.join(snd_dir, 'boom.aiff'))
+
+    # joystick
+
+    try:
+       pygame.joystick.init()
+       joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+       joysticks[0].init()
+       joysticks[1].init()
+       player1_joystick = joysticks[0]
+       player2_joystick = joysticks[1]
+    except IndexError:
+       player1_joystick = None
+       player2_joystick = None
+
+    """while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            elif event.type == pygame.locals.JOYAXISMOTION:
+                player1jx, player1jy = player1_joystick.get_axis(0), player1_joystick.get_axis(1)
+                if player1jx < 0:
+                    print("left")
+                if player1jx > 0:
+                    print("right")
+                if player1jy < 0:
+                    print("up")
+                if player1jy > 0:
+                    print("down")
+            elif event.type == pygame.JOYBUTTONDOWN:
+                print(event)"""
+
+
+    while not done:
+        # --- Event Processing
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    player.move_down()
+                if event.key == pygame.K_UP:
+                    player.move_up()
+                if event.key == pygame.K_LEFT:
+                    player.move_left()
+                if event.key == pygame.K_RIGHT:
+                    player.move_right()
+
+                if event.key == pygame.K_s:
+                    player2.move_down()
+                if event.key == pygame.K_w:
+                    player2.move_up()
+                if event.key == pygame.K_a:
+                    player2.move_left()
+                if event.key == pygame.K_d:
+                    player2.move_right()
+
+                if event.key == pygame.K_SPACE:
+                    bullet = Bullet(player.rect.centerx, player.rect.centery, player.dirvect, player.speed)
+                    bullet2 = Bullet(player2.rect.centerx, player2.rect.centery, player2.dirvect, player2.speed)
+                    bullet_list.add(bullet, bullet2)
+                    all_sprites_list.add(bullet, bullet2)
+                    p1_bullet.add(bullet)
+                    p2_bullet.add(bullet2)
+                    shoot_snd.play(loops=0)
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN or event.key == pygame.K_UP or event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    player.dirvect[1] = 0
+
+                if event.key == pygame.K_s or event.key == pygame.K_w or event.key == pygame.K_a or event.key == pygame.K_d:
+                    player2.dirvect[1] = 0
+
+            if event.type == pygame.locals.JOYAXISMOTION:
+                player1jx, player1jy = player1_joystick.get_axis(0), player1_joystick.get_axis(1)
+                if player1jx < 0 and player1jy == 0 :
+                    player.move_left()
+                if player1jx > 0 and player1jy == 0:
+                    player.move_right()
+                if player1jy < 0 and player1jx == 0:
+                    player.move_up()
+                if player1jy > 0 and player1jx == 0:
+                    player.move_down()
+                if player1jx == 0 and player1jy == 0:
+                    player.dirvect[1] = 0
+                if player1jx < 0 and player1jy < 0:
+                    player.move_dl()
+                if player1jx > 0 and player1jy < 0:
+                    player.move_dr()
+                if player1jx < 0 and player1jy > 0:
+                    player.move_ul()
+                if player1jx > 0 and player1jy > 0:
+                    player.move_ur()
+
+            if event.type == pygame.locals.JOYAXISMOTION:
+                player2jx, player2jy = player2_joystick.get_axis(0), player2_joystick.get_axis(1)
+                if player2jx < 0 and player2jy == 0 :
+                    player2.move_left()
+                if player2jx > 0 and player2jy == 0:
+                    player2.move_right()
+                if player2jy < 0 and player2jx == 0:
+                    player2.move_up()
+                if player2jy > 0 and player2jx == 0:
+                    player2.move_down()
+                if player2jx == 0 and player2jy == 0:
+                    player2.dirvect[1] = 0
+                if player2jx < 0 and player2jy < 0:
+                    player2.move_dl()
+                if player2jx > 0 and player2jy < 0:
+                    player2.move_dr()
+                if player2jx < 0 and player2jy > 0:
+                    player2.move_ul()
+                if player2jx > 0 and player2jy > 0:
+                    player2.move_ur()
+
+            if event.type == pygame.JOYBUTTONDOWN:
+                if player1_joystick.get_button(0):
+                    bullet = Bullet(player.rect.centerx, player.rect.centery, player.dirvect, player.speed)
+                    p1_bullet.add(bullet)
+                    all_sprites_list.add(bullet)
+                    bullet_list.add(bullet)
+                    shoot_snd.play(loops=0)
+
+                if player2_joystick.get_button(0):
+                    bullet2 = Bullet(player2.rect.centerx, player2.rect.centery, player2.dirvect, player2.speed)
+                    p2_bullet.add(bullet2)
+                    bullet_list.add(bullet2)
+                    all_sprites_list.add(bullet2)
+                    shoot_snd.play(loops=0)
+
+        all_sprites_list.update()
+
+        if pygame.sprite.spritecollide(player, p2_bullet, True):
+            boom1 = Boom(player.rect.centerx, player.rect.centery)
+            player.kill()
+            #screen.blit(boom1.image, boom1.rect)
+            all_sprites_list.add(boom1)
+            boom_snd.play(loops=0)
+
+        if pygame.sprite.spritecollide(player2, p1_bullet, True):
+            boom2 = Boom(player2.rect.centerx, player2.rect.centery)
+            #screen.blit(boom2.image, boom2.rect)
+            boom_snd.play(loops=0)
+            all_sprites_list.add(boom2)
+            player2.kill()
+
+        if pygame.sprite.groupcollide(bullet_list,wall_list,True,False):
+            pass
 
 
 
+        for bullet in bullet_list:
+            if bullet.rect.y < -10:
+                bullet_list.remove(bullet)
+                all_sprites_list.remove(bullet)
 
-wall_list = pygame.sprite.Group()
-all_sprites_list = pygame.sprite.Group()
-p1_bullet = pygame.sprite.Group()
-p2_bullet = pygame.sprite.Group()
-bullet_list = pygame.sprite.Group()
+        screen.fill([50, 50, 50])
+        screen.blit(BackGround.image, BackGround.rect)
+        # Draw all the spites
+        all_sprites_list.draw(screen)
 
-player = Player(50,screen_height/2, [0, 4], 1)
-player2 = Player(screen_width-50,screen_height/2, [180, 4], 2)
-all_sprites_list.add(player,player2)
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
 
+        # --- Limit to 20 frames per second
+        clock.tick(30)
 
-rock_wall = Wall(screen_width/2, screen_height/2,100,100)
-wall_list.add(rock_wall)
-all_sprites_list.add(rock_wall)
-
-
-
-
-
-done = False
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-pygame.key.set_repeat(50,50)
-
-score = 0
-
-# sound
-shoot_snd = pygame.mixer.Sound(path.join(snd_dir, 'shoot.wav'))
-boom_snd = pygame.mixer.Sound(path.join(snd_dir, 'boom.aiff'))
-
-# joystick
-
-try:
-   pygame.joystick.init()
-   joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-   joysticks[0].init()
-   joysticks[1].init()
-   player1_joystick = joysticks[0]
-   player2_joystick = joysticks[1]
-except IndexError:
-   player1_joystick = None
-   player2_joystick = None
-
-"""while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit()
-        elif event.type == pygame.locals.JOYAXISMOTION:
-            player1jx, player1jy = player1_joystick.get_axis(0), player1_joystick.get_axis(1)
-            if player1jx < 0:
-                print("left")
-            if player1jx > 0:
-                print("right")
-            if player1jy < 0:
-                print("up")
-            if player1jy > 0:
-                print("down")
-        elif event.type == pygame.JOYBUTTONDOWN:
-            print(event)"""
-
-
-while not done:
-    # --- Event Processing
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                player.move_down()
-            if event.key == pygame.K_UP:
-                player.move_up()
-            if event.key == pygame.K_LEFT:
-                player.move_left()
-            if event.key == pygame.K_RIGHT:
-                player.move_right()
-
-            if event.key == pygame.K_s:
-                player2.move_down()
-            if event.key == pygame.K_w:
-                player2.move_up()
-            if event.key == pygame.K_a:
-                player2.move_left()
-            if event.key == pygame.K_d:
-                player2.move_right()
-
-            if event.key == pygame.K_SPACE:
-                bullet = Bullet(player.rect.centerx, player.rect.centery, player.dirvect, player.speed)
-                bullet2 = Bullet(player2.rect.centerx, player2.rect.centery, player2.dirvect, player2.speed)
-                bullet_list.add(bullet, bullet2)
-                all_sprites_list.add(bullet, bullet2)
-                p1_bullet.add(bullet)
-                p2_bullet.add(bullet2)
-                shoot_snd.play(loops=0)
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_DOWN or event.key == pygame.K_UP or event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                player.dirvect[1] = 0
-
-            if event.key == pygame.K_s or event.key == pygame.K_w or event.key == pygame.K_a or event.key == pygame.K_d:
-                player2.dirvect[1] = 0
-
-        if event.type == pygame.locals.JOYAXISMOTION:
-            player1jx, player1jy = player1_joystick.get_axis(0), player1_joystick.get_axis(1)
-            if player1jx < 0 and player1jy == 0 :
-                player.move_left()
-            if player1jx > 0 and player1jy == 0:
-                player.move_right()
-            if player1jy < 0 and player1jx == 0:
-                player.move_up()
-            if player1jy > 0 and player1jx == 0:
-                player.move_down()
-            if player1jx == 0 and player1jy == 0:
-                player.dirvect[1] = 0
-            if player1jx < 0 and player1jy < 0:
-                player.move_dl()
-            if player1jx > 0 and player1jy < 0:
-                player.move_dr()
-            if player1jx < 0 and player1jy > 0:
-                player.move_ul()
-            if player1jx > 0 and player1jy > 0:
-                player.move_ur()
-
-        if event.type == pygame.locals.JOYAXISMOTION:
-            player2jx, player2jy = player2_joystick.get_axis(0), player2_joystick.get_axis(1)
-            if player2jx < 0 and player2jy == 0 :
-                player2.move_left()
-            if player2jx > 0 and player2jy == 0:
-                player2.move_right()
-            if player2jy < 0 and player2jx == 0:
-                player2.move_up()
-            if player2jy > 0 and player2jx == 0:
-                player2.move_down()
-            if player2jx == 0 and player2jy == 0:
-                player2.dirvect[1] = 0
-            if player2jx < 0 and player2jy < 0:
-                player2.move_dl()
-            if player2jx > 0 and player2jy < 0:
-                player2.move_dr()
-            if player2jx < 0 and player2jy > 0:
-                player2.move_ul()
-            if player2jx > 0 and player2jy > 0:
-                player2.move_ur()
-
-        if event.type == pygame.JOYBUTTONDOWN:
-            if player1_joystick.get_button(0):
-                bullet = Bullet(player.rect.centerx, player.rect.centery, player.dirvect, player.speed)
-                p1_bullet.add(bullet)
-                all_sprites_list.add(bullet)
-                bullet_list.add(bullet)
-                shoot_snd.play(loops=0)
-
-            if player2_joystick.get_button(0):
-                bullet2 = Bullet(player2.rect.centerx, player2.rect.centery, player2.dirvect, player2.speed)
-                p2_bullet.add(bullet2)
-                bullet_list.add(bullet2)
-                all_sprites_list.add(bullet2)
-                shoot_snd.play(loops=0)
-
-    all_sprites_list.update()
-
-    if pygame.sprite.spritecollide(player, p2_bullet, True):
-        boom1 = Boom(player.rect.centerx, player.rect.centery)
-        player.kill()
-        #screen.blit(boom1.image, boom1.rect)
-        all_sprites_list.add(boom1)
-        boom_snd.play(loops=0)
-
-    if pygame.sprite.spritecollide(player2, p1_bullet, True):
-        boom2 = Boom(player2.rect.centerx, player2.rect.centery)
-        #screen.blit(boom2.image, boom2.rect)
-        boom_snd.play(loops=0)
-        all_sprites_list.add(boom2)
-        player2.kill()
-
-    if pygame.sprite.groupcollide(bullet_list,wall_list,True,False):
-        pass
-
-
-
-    for bullet in bullet_list:
-        if bullet.rect.y < -10:
-            bullet_list.remove(bullet)
-            all_sprites_list.remove(bullet)
-
-    screen.fill([50, 50, 50])
-    screen.blit(BackGround.image, BackGround.rect)
-    # Draw all the spites
-    all_sprites_list.draw(screen)
-
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-
-    # --- Limit to 20 frames per second
-    clock.tick(30)
-
-pygame.quit()
+    pygame.quit()
---
+++ menu.py
@@ -0,0 +1,91 @@
+import pygame
+import sys
+from main_joy import *
+
+pygame.init()
+
+
+class GameMenu():
+    def __init__(self, screen, items, bg_color=(0, 0, 0), font=None, font_size=50,
+                 font_color=(255, 255, 255)):
+        self.screen = screen
+        self.scr_width = self.screen.get_rect().width
+        self.scr_height = self.screen.get_rect().height
+        self.bg_color = bg_color
+        self.clock = pygame.time.Clock()
+        self.items = items
+        self.font = pygame.font.SysFont(font, font_size)
+        self.font_color = font_color
+
+        self.items = []
+        for index, item in enumerate(items):
+            label = self.font.render(item, 1, font_color)
+
+            width = label.get_rect().width
+            height = label.get_rect().height
+
+            posx = (self.scr_width / 2) - (width / 2)
+            # t_h: total height of text block
+            t_h = len(items) * height
+            posy = (self.scr_height / 2) - (t_h / 2) + (index * height)
+
+            self.items.append([item, label, (width, height), (posx, posy)])
+
+    def run(self):
+        mainloop = True
+        while mainloop:
+            # Limit frame speed to 50 FPS
+            self.clock.tick(50)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    mainloop = False
+
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_0:
+                    done = True
+                    sys.exit(0)
+
+
+            pressed = pygame.key.get_pressed()
+            # Up
+            if pressed[pygame.K_1]:
+                main()
+
+            # Down
+            if pressed[pygame.K_2]:
+                running = False
+                sys.exit(0)
+
+            if event.type == pygame.JOYBUTTONDOWN:
+                if player1_joystick.get_button():
+                    main()
+                if player2_joystick.get_button(0):
+                    main()
+            if event.type == pygame.JOYBUTTONUP:
+                if player1_joystick.get_button():
+                    running = False
+                    sys.exit(0)
+                if player2_joystick.get_button():
+                    running = False
+                    sys.exit(0)
+
+
+
+            # Redraw the background
+            self.screen.fill(self.bg_color)
+
+            for name, label, (width, height), (posx, posy) in self.items:
+                self.screen.blit(label, (posx, posy))
+
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    # Creating the screen
+    screen = pygame.display.set_mode((1280, 800), 0, 32)
+
+    menu_items = ('Start', 'Quit')
+
+    pygame.display.set_caption('Game Menu')
+    gm = GameMenu(screen, menu_items)
+    gm.run()
