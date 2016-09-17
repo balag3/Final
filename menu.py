550223709ace3d31e3f18f6eeb9b1242b95b7528
lorszil <lorszil@gmail.com>
2016-09-17 11:34:54 +0200

    menut test3.0

--- menu.py
+++ menu.py
@@ -50,10 +50,10 @@ class GameMenu():
                joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
                joysticks[0].init()
                joysticks[1].init()
-               player1_joystick = joysticks[0]
+               player_joystick = joysticks[0]
                player2_joystick = joysticks[1]
             except IndexError:
-               player1_joystick = None
+               player_joystick = None
                player2_joystick = None
 
 
@@ -69,7 +69,7 @@ class GameMenu():
 
 
             if event.type == pygame.JOYBUTTONDOWN:
-                if player1_joystick.get_button(0):
+                if player_joystick.get_button(0):
                     running = False
                     sys.exit(0)
                 if player2_joystick.get_button(0):
@@ -77,7 +77,7 @@ class GameMenu():
                     sys.exit(0)
 
             if event.type == pygame.JOYBUTTONUP:
-                if player1_joystick.get_button(0):
+                if player_joystick.get_button(0):
                     main()
                 if player2_joystick.get_button(0):
                     main()
